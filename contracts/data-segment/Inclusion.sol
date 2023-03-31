9b88a2ee0229c21675badabfedf1557a4d22f6b3
luc <luc.roy@gmail.com>
2023-03-31 11:48:40 -0300

    added computeRoot function

--- contracts/data-segment/Inclusion.sol
+++ contracts/data-segment/Inclusion.sol
@@ -12,4 +12,22 @@ contract Inclusion {
         return MerkleProof.verify(proof, root, leaf);
     }
 
+    function computeRoot(bytes32 leaf, bytes32[] memory proof, uint256 height) public pure returns (bytes32) {
+        require(proof.length == height, "Invalid proof length");
+
+        bytes32 currentHash = leaf;
+
+        for (uint256 i = 0; i < proof.length; i++) {
+            bytes32 siblingHash = proof[i];
+
+            if ((height >> i) & 1 == 1) {
+                currentHash = keccak256(abi.encodePacked(siblingHash, currentHash));
+            } else {
+                currentHash = keccak256(abi.encodePacked(currentHash, siblingHash));
+            }
+        }
+
+        return currentHash;
+    }
+
 }
