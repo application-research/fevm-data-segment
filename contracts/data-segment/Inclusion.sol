01ac30284909a187a72e94d7d68ab932603bb1ef
luc <luc.roy@gmail.com>
2023-04-04 20:49:23 -0300

    changed to sha256

--- contracts/data-segment/Inclusion.sol
+++ contracts/data-segment/Inclusion.sol
@@ -4,37 +4,46 @@ pragma solidity ^0.8.9;
 // Uncomment this line to use console.log
 import "hardhat/console.sol";
 
-import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
-
 contract Inclusion {
 
-    function verify(bytes32 root, bytes32 leaf, bytes32[] memory proof) public pure returns (bool) {
-        return MerkleProof.verify(proof, root, leaf);
+    struct Node {
+        bytes32 data;
     }
 
     struct ProofData {
         uint256 index;
-        bytes32[] path;
+        Node[] path;
     }
 
-    function computeRoot(ProofData memory d, bytes32 subtree) public pure returns (bytes32) {
+    function computeRoot(ProofData memory d, Node memory subtree) public pure returns (Node memory) {
         require(d.path.length < 64, "merkleproofs with depths greater than 63 are not supported");
         require(d.index >> d.path.length == 0, "index greater than width of the tree");
         
-        bytes32 carry = subtree;
+        Node memory carry = subtree;
         uint256 index = d.index;
         uint256 right = 0;
 
         for (uint256 i = 0; i < d.path.length; i++) {
             (right, index) = (index & 1, index >> 1);
             if (right == 1) {
-                carry = keccak256(abi.encodePacked(d.path[i], carry));
+                carry = truncate(computeNode(d.path[i], carry));
             } else {
-                carry = keccak256(abi.encodePacked(carry, d.path[i]));
+                carry = truncate(computeNode(carry, d.path[i]));
             }
         }
 
         return carry;
+    }
+
+    function computeNode(Node memory left, Node memory right) internal pure returns (Node memory) {
+        bytes memory data = abi.encodePacked(left.data, right.data);
+        return Node(sha256(data));
+    }
+
+    function truncate(Node memory n) public pure returns (Node memory) {
+        // Clear the two most significant bits of the last byte
+        n.data &= bytes32(0x3fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff);
+        return n;
     }
 
 }
--- test/InclusionTest.js
+++ test/InclusionTest.js
@@ -23,32 +23,6 @@ describe("Inclusion Tests", function () {
         this.inclusion = await deploy('Inclusion');
     });
 
-    it("leaf 'G' is part of root", async function () {
-        const merkleTree = createMerkleTree()
-        const root = merkleTree.getHexRoot();
-        const leaf = keccak256('G');
-        const proof = merkleTree.getHexProof(leaf);
-
-        console.log("root : " + root);
-        console.log("leaf : " + leaf);
-        console.log("proof : " + proof);
-
-        expect(await this.inclusion.verify(root, leaf, proof)).to.equal(true);
-    });
-
-    it("leaf 'g' is not part of root", async function () {
-        const merkleTree = createMerkleTree()
-        const root = merkleTree.getHexRoot();
-        const leaf = web3.utils.sha3('G');
-        const proof = merkleTree.getHexProof(leaf);
-
-        console.log("root : " + root);
-        console.log("leaf : " + leaf);
-        console.log("proof : " + proof);
-        
-        expect(await this.inclusion.verify(root, keccak256('g'), proof)).to.not.equal(true);
-    });
-
     it("Compute Root", async function () {
 
         //  subtree: Node{0x1},
@@ -59,20 +33,35 @@ describe("Inclusion Tests", function () {
 		// 		0x65, 0xa1, 0xbd, 0xd3, 0x1b, 0x4a, 0xcc, 0x4c, 0x79, 0x12, 0x1f, 0x2e, 0x1b, 0xa8,
 		// 		0x48, 0x7d, 0x1f, 0x30},
 
+        // const proof = {
+        //     index: 0,
+        //     path: [
+        //       Buffer.alloc(32).fill(0x2),
+        //       Buffer.alloc(32).fill(0x3),
+        //     ],
+        // };
+          
+        // const subtree = Buffer.alloc(32).fill(0x1);
+
+
         const proof = {
             index: 0,
             path: [
-              "0x0200000000000000000000000000000000000000000000000000000000000000",
-              "0x0300000000000000000000000000000000000000000000000000000000000000",
+              { data: "0x0200000000000000000000000000000000000000000000000000000000000000" },
+              { data: "0x0300000000000000000000000000000000000000000000000000000000000000" },
             ],
         };
           
-        const subtree = "0x0100000000000000000000000000000000000000000000000000000000000000";
+        const subtree = {
+            data: "0x0100000000000000000000000000000000000000000000000000000000000000"
+        }
+
         const expectedRoot = "0xaa9627470b129fab0db1260da80065a1bdd31b4acc4c79121f2e1ba8487d1f30";  
 
         const computeRoot = await this.inclusion.computeRoot(proof, subtree);
         
-        expect(computeRoot).to.equal(expectedRoot);
+        expect(computeRoot.data).to.equal(expectedRoot);
+        // expect(true).to.equal(true);
     });
 
 });
