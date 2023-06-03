e985407fbbdc4596444ea3288b4f73b4fffc34d9
Jay Logelin <lodge@protocol.ai>
2023-06-03 11:43:39 -0300

    updated event response code

--- fdt-deal-monitor-ethers.js
+++ fdt-deal-monitor-ethers.js
@@ -1,6 +1,7 @@
 const FormData = require('form-data');
 const axios = require('axios');
 const { ethers } = require('hardhat');
+const CIDTool = require('cid-tool')
 
 require('dotenv').config();
 
@@ -14,13 +15,10 @@ async function waitForNonZeroDealId(url) {
   while (dealId === 0) {
     try {
       const response = await axios.get(url);
-
-      console.log(response.data);
-
       const payload = response.data;
       
-      if (payload.data.deal_info.deal_id != 0 ) {
-        
+      if (payload.data.deal_info.deal_id == 0 ) {
+      // if (payload.data.deal_info.deal_id != 0 ) {
         return payload;
       }
       console.log('waiting to request again in 5 minutes');
@@ -38,18 +36,58 @@ function ipfsCidToHex(ipfsCid) {
   return rval.substr(1, rval.length - 1);
 }
 
-async function main() {
+async function tester() {
+
+  // Get args
+  const rpcEndpoint = process.env.rpcEndpoint;
+  const apiEndpoint = process.env.apiEndpoint + "/content/add";
+  const contractAddress = process.env.contractAddress;
+  const cidContractAddress = process.env.contractAddress;
+  const API_KEY = process.env.API_KEY;
+
+  // Create a new ethers provider
+  const provider = new ethers.providers.JsonRpcProvider(rpcEndpoint);
+
+  // Compile the contract
+  const contractName = 'EdgeAggregatorOracle';
+  const Cid = await ethers.getContractFactory("Cid");
+  // const contractFactory = await ethers.getContractFactory(contractName);
+  const contractFactory = await ethers.getContractFactory(contractName, {
+    libraries: {
+      Cid: cidContractAddress,
+    },
+  });
+  
+  const contract = await contractFactory.attach(contractAddress)
+
   try {
     const url = 'https://hackfs.edge.estuary.tech/open/status/content/10'; // Replace with your URL
     const res = await waitForNonZeroDealId(url)
     console.log('Non-zero deal_id:', res);
+    executecompletionCallback(res, contract);
   } catch (error) {
     console.error('Error:', error);
   }
 }
 
-async function main1() {
+async function executecompletionCallback(input, aggregator) {
+  verifData = {
+    commPc: "0x" + ipfsCidToHex(input.data.sub_piece_info.comm_pc),
+    sizePc: input.data.sub_piece_info.size_pc,
+  }
+  incProof = {
+      proofSubtree: {
+          index: input.data.sub_piece_info.inclusion_proof.proofSubtree.index,
+          path: input.data.sub_piece_info.inclusion_proof.proofSubtree.path
+      },
 
+      proofIndex:{
+          index: input.data.sub_piece_info.inclusion_proof.proofIndex.index,
+          path: input.data.sub_piece_info.inclusion_proof.proofIndex.path
+      },
+  }
+  resp = await aggregator.complete(1, 1234, incProof, verifData);
+  console.log(resp);
 }
 
 async function main2() {
@@ -130,6 +168,13 @@ async function main2() {
     try {
       const response = await axios.post(apiEndpoint, formData, postHeaders);
       console.log('API response:', response.data);
+
+      const url = 'https://hackfs.edge.estuary.tech/open/status/content/' + response.contents.ID;
+      const res = await waitForNonZeroDealId(url);
+      console.log('Non-zero deal_id:', res);
+
+      // call completion callback
+      executecompletionCallback(res);
     } catch (error) {
       console.error('API request failed:', error.message);
     }
